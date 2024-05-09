62407882e7ae274f533ea88728f5f939c9b83857
Portatil <gazagnealejo@gmail.com>
2024-05-08 22:44:22 -0300

    iniciando back

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const router = require('./router/router.js')
+import express, { json } from 'express'
+import cors from 'cors'
+import router from './router/router.js'
 const app = express()
-app.use(express.json())
+app.use(json())
 app.use(cors())
 app.disable('x-powered-by')
 const desiredPort = process.env.PORT ?? 1234
 
 app.get('/', (req, res) => {
     res.send('Hello word')
-})
-
-app.get('/', (req, res) => {
-
 })
 
 app.listen(desiredPort, () => {
