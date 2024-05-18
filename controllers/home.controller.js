3a2d7e2b50039ec6b9f67fd08cda61a6d122b2ba
Pandulc <gabiguibe@gmail.com>
2024-05-18 14:04:04 -0300

    some changes

---
+++ controllers/home.controller.js
@@ -0,0 +1,21 @@
+const PostServices = require("../services/post.services");
+const postServices = new PostServices();
+
+const getThreePosts = async (req, res) => {
+  try {
+    let posts = await postServices.getThreePost();
+    for (let i = 0; i < posts.length; i++) {
+      delete posts[i].images;
+      delete posts[i].area;
+      delete posts[i].sellerId;
+      delete posts[i].published;
+      delete posts[i].ubication;
+    }
+    return res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error al obtener propiedades" });
+  }
+};
+
+module.exports = { getThreePosts };
---
+++ services/post.services.js
@@ -0,0 +1,210 @@
+const { PrismaClient } = require("@prisma/client");
+
+class Post {
+  constructor() {}
+
+  async getPostById(id) {
+    const prisma = new PrismaClient();
+    const post = await prisma.post.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return post;
+  }
+
+  async getThreePost() {
+    const prisma = new PrismaClient();
+    const posts = await prisma.post.findMany({
+      take: 3,
+      where: {
+        published: true,
+      },
+    });
+    return posts;
+  }
+
+  async getMyPosts(id) {
+    const prisma = new PrismaClient();
+    const posts = await prisma.post.findMany({
+      where: {
+        sellerId: id,
+      },
+    });
+    return posts;
+  }
+
+  async getSomePost() {
+    const prisma = new PrismaClient();
+    const posts = await prisma.post.findMany({
+      take: 6,
+      where: {
+        published: true,
+      },
+    });
+    return posts;
+  }
+
+  async getPosts(body) {
+    // const searchParams = {
+    //   name: body?.name || '',
+    //   price: body?.price || undefined,
+    //   ubication: body?.ubication || '',
+    //   type: body?.type || '',
+    //   rooms: body?.rooms || undefined,
+    //   bathrooms: body?.bathrooms || undefined,
+    //   garage: body?.garage || undefined,
+    //   area: body?.area || undefined,
+    //   onSale: body?.onSale || undefined,
+    // };
+
+    // const where = {};
+
+    // for (const [key, value] of Object.entries(searchParams)) {
+    //   if (value !== undefined && value !== '') {
+    //     where[key] = { contains: value };
+    //   }
+    // }
+
+    // if (!searchParams.price) {
+    //   where.OR = [{ price: { _eq: null } }];
+    // }
+    // const prisma = new PrismaClient();
+    // const posts = await prisma.post.findMany({
+    //   where: where,
+    //   published: true
+    // });
+    // return posts;
+
+    const {
+      name,
+      price,
+      ubication,
+      type,
+      rooms,
+      bathrooms,
+      garage,
+      area,
+      onSale,
+    } = body;
+    const prisma = new PrismaClient();
+    const posts = await prisma.post.findMany({
+      where: {
+        title: {
+          contains: name,
+        },
+        price: {
+          gte: price * 0.85,
+          lte: price * 1.15,
+        },
+        ubication: {
+          contains: ubication,
+        },
+        type: {
+          contains: type,
+        },
+        rooms: rooms,
+        bathrooms: bathrooms,
+        garage: garage,
+        area: area,
+        onSale: onSale,
+        published: true,
+      },
+    });
+    return posts;
+  }
+
+  async createPost(body) {
+    const {
+      title,
+      content,
+      published,
+      price,
+      onSale,
+      ubication,
+      frontImage,
+      images,
+      type,
+      rooms,
+      bathrooms,
+      garage,
+      area,
+      pool,
+      pets,
+      seller,
+      sellerId,
+    } = body;
+
+    const prisma = new PrismaClient();
+    const post = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        published: published,
+        price: price,
+        onSale: onSale,
+        ubication: ubication,
+        frontImage: frontImage,
+        images: images,
+        type: type,
+        rooms: rooms,
+        bathrooms: bathrooms,
+        garage: garage,
+        area: area,
+        pool: pool,
+        pets: pets,
+        seller: seller,
+        sellerId: sellerId,
+      },
+    });
+    return post;
+  }
+
+  async updatePost(body) {
+    const {
+      id,
+      title,
+      content,
+      published,
+      price,
+      onSale,
+      ubication,
+      frontImage,
+      images,
+      type,
+      rooms,
+      bathrooms,
+      garage,
+      area,
+      pool,
+      pets,
+    } = body;
+
+    const prisma = new PrismaClient();
+    const post = await prisma.post.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title: title,
+        content: content,
+        published: published,
+        price: price,
+        onSale: onSale,
+        ubication: ubication,
+        frontImage: frontImage,
+        images: images,
+        type: type,
+        rooms: rooms,
+        bathrooms: bathrooms,
+        garage: garage,
+        area: area,
+        pool: pool,
+        pets: pets,
+      },
+    });
+    return post;
+  }
+}
+
+module.exports = Post;
