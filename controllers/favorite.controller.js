7836a68f5bf0259346308843e7a6d075b84b9ab5
Alejo Gazagne <gazagnealejo@gmail.com>
2024-05-25 19:16:15 -0300

    chore: Update Post service to include datetime in updatePost method

---
+++ controllers/favorite.controller.js
@@ -0,0 +1,31 @@
+const FavoriteService = require('../services/favorites.services');
+const favoriteService = new FavoriteService();
+const PostService = require('../services/post.services');
+const postService = new PostService();
+
+const get = async (req, res) => {
+  try {
+    const favs = await favoriteService.getFavorites(req.token.id);
+    let response = []
+    for (let i = 0; i < favs.length; i++) {
+      tmp = await postService.getPostById(favs[i].postId);
+      response.push(tmp);
+    }
+    res.json({ message: 'Favorites found', data: response });
+  } catch (error) {
+    res.status(500).send({ message: 'Internal server error' });
+  }
+}
+
+const create = async (req, res) => {
+  try {
+    req.body.userId = req.token.id;
+    //console.log(req.body);
+    const response = await favoriteService.createFavorite(req.body);
+    res.json({ message: 'Favorite created', data: response });
+  } catch (error) {
+    res.status(500).send({ message: 'Internal server error' });
+  }
+}
+
+module.exports = { get, create };
--- prisma/migrations/20240522195403_bdd/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `datetime` to the `Post` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "datetime" TIMESTAMP(3) NOT NULL;
